b0ff24fc9e70313026d18c86bab5e6a05a45971c
Roi Revach <roi.revach1@gmail.com>
2025-06-21 16:45:25 +0300

    babel configuration update

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,14 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      [
-        'module-resolver',
-        {
-          alias: {
-            '@': './',
-          },
+      ['module-resolver', {
+        alias: {
+          '@': './',
         },
-      ],
+      }],
+      'nativewind/babel',
     ],
   };
 };
